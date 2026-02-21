067787cc3b372312b67e9e3ae731c126842fce80
Sean Rogowsky <rogowskys@gmail.com>
2026-02-21 16:18:24 -0600

    Changes to be committed: 	modified:   requirements.txt 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -17,10 +17,10 @@ sudo apt-get update
 sudo apt-get install -y python3 python3-pip python3-venv \
     libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev \
     pkg-config libgl1-mesa-dev libgles2-mesa-dev \
-    python3-setuptools libgstreamer1.0-dev git-core \
+    python3-setuptools libgstreamer1.0-dev git \
     gstreamer1.0-plugins-bad gstreamer1.0-plugins-base \
     gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly \
-    gstreamer1.0-omx gstreamer1.0-alsa python3-dev libmtdev-dev \
+    gstreamer1.0-alsa gstreamer1.0-gl python3-dev libmtdev-dev \
     xclip xsel libjpeg-dev unclutter x11-xserver-utils
 
 echo "Step 1b: Configuring kiosk mode settings..."
